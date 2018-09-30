d398d14420f45837fb24b2fe537f70f553f6d2a1
yangtongbin <yang_t_b@qq.com>
2018-09-30 16:41:25 +0800

    疾病目录

---
+++ open-core-facade/src/main/java/com/sm/open/core/facade/model/result/pf/biz/disease/PfDiseaseZtreeResult.java
@@ -0,0 +1,27 @@
+package com.sm.open.core.facade.model.result.pf.biz.disease;
+
+import com.sm.open.core.facade.model.result.pf.biz.PfCommonZtreeResult;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@ToString
+public class PfDiseaseZtreeResult extends PfCommonZtreeResult implements Serializable {
+
+    private static final long serialVersionUID = -4901737093643963963L;
+
+    /**
+     * 疾病目录ID
+     */
+    private Long        idDieclass;
+
+    /**
+     * 激活标志
+     */
+    private String fgActive;
+
+}
--- open-core-service-pf/src/main/java/com/sm/open/core/service/facade/pf/biz/disease/PfDiseaseConstant.java
+++ open-core-service-pf/src/main/java/com/sm/open/core/service/facade/pf/biz/disease/PfDiseaseConstant.java
@@ -2,16 +2,32 @@ package com.sm.open.core.service.facade.pf.biz.disease;
 
 public class PfDiseaseConstant {
 
-    public static final String PAGE_DISEASE_INFO_LIST_ERROR           = "pageDiseaseInfoListError";
-    public static final String PAGE_DISEASE_INFO_LIST_ERROR_MSG       = "分页查询疾病信息列表失败";
 
-    public static final String ADD_DISEASE_INFO_ERROR                 = "addDiseaseInfoFailed";
-    public static final String ADD_DISEASE_INFO_ERROR_MSG             = "新增疾病信息失败";
+    public static final String LIST_DISEASE_CATALOGUE_TREE_ERROR            = "listDiseaseCatalogueTreeError";
+    public static final String LIST_DISEASE_CATALOGUE_TREE_ERROR_MSG        = "查询疾病目录失败";
 
-    public static final String EDIT_DISEASE_INFO_ERROR                = "editDiseaseInfoFailed";
-    public static final String EDIT_DISEASE_INFO_ERROR_MSG            = "编辑疾病信息失败";
+    public static final String SELECT_DISEASE_DETAIL_ERROR                  = "selectDiseaseDetailError";
+    public static final String SELECT_DISEASE_DETAIL_ERROR_MSG              = "查询疾病信息详情失败";
 
-    public static final String DEL_DISEASE_INFO_ERROR                 = "delDiseaseInfoFailed";
-    public static final String DEL_DISEASE_INFO_ERROR_MSG             = "删除疾病信息失败";
+    public static final String PAGE_DISEASE_INFO_LIST_ERROR                 = "pageDiseaseInfoListError";
+    public static final String PAGE_DISEASE_INFO_LIST_ERROR_MSG             = "分页查询疾病信息列表失败";
+
+    public static final String EXIST_DISEASE_CATALOGUE_ERROR                = "existDiseaseCatalogueFailed";
+    public static final String EXIST_DISEASE_CATALOGUE_ERROR_MSG            = "已存在该疾病目录";
+
+    public static final String SAVE_DISEASE_CATALOGUE_ERROR                 = "saveDiseaseCatalogueFailed";
+    public static final String SAVE_DISEASE_CATALOGUE_ERROR_MSG             = "新增疾病目录失败";
+
+    public static final String DEL_DISEASE_CATALOGUE_ERROR                  = "delDiseaseCatalogueFailed";
+    public static final String DEL_DISEASE_CATALOGUE_ERROR_MSG              = "删除疾病目录失败";
+
+    public static final String ADD_DISEASE_INFO_ERROR                       = "addDiseaseInfoFailed";
+    public static final String ADD_DISEASE_INFO_ERROR_MSG                   = "新增疾病信息失败";
+
+    public static final String EDIT_DISEASE_INFO_ERROR                      = "editDiseaseInfoFailed";
+    public static final String EDIT_DISEASE_INFO_ERROR_MSG                  = "编辑疾病信息失败";
+
+    public static final String DEL_DISEASE_INFO_ERROR                       = "delDiseaseInfoFailed";
+    public static final String DEL_DISEASE_INFO_ERROR_MSG                   = "删除疾病信息失败";
     
 }
