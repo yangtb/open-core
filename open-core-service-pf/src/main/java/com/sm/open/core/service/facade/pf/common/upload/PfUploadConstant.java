43629d26de3dbfd728e10c69f55697c0db7d5642
yangtongbin <yang_t_b@qq.com>
2018-10-07 19:45:37 +0800

    上传

---
+++ open-core-service-pf/src/main/java/com/sm/open/core/service/facade/pf/common/upload/PfUploadConstant.java
@@ -0,0 +1,8 @@
+package com.sm.open.core.service.facade.pf.common.upload;
+
+public class PfUploadConstant {
+
+    public static final String ADD_MEDIA_INFO_ERROR         = "addMediaInfoFailed";
+    public static final String ADD_MEDIA_INFO_ERROR_MSG     = "保存多媒体信息失败";
+
+}
