9c78bb3ca3c3e743a5f1d27fe91f82df38d74a13
yangtb <yang_t_b@163.com>
2018-10-26 19:01:42 +0800

    修改脚本

--- open-core-service/src/main/resources/run.sh
+++ open-core-service/src/main/resources/run.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
-  
-SERVER=/opt/software/opencore
+
+
+#获取当前目录的上一级目录
+SERVER=$(cd `dirname $0`;cd ..;pwd)
+
 cd $SERVER
+JARNAME=`ls -t|grep ".jar"|head -1|awk '{print $NF}'`
+JARNAME=${JARNAME%.*}
 
-case "$1" in  
+if [ -z "$JAVA_PARAMS" ] ; then
+	JAVA_PARAMS="-Xmx384m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SERVER -Xloggc:$SERVER/gc_log_file.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Djava.security.egd=file:/dev/./urandom -XX:AutoBoxCacheMax=20000 -XX:+PrintCommandLineFlags"
+fi
 
-  start)  
-    nohup java -Xmx384m -jar opencore.jar -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=63323,server=y,suspend=n >/dev/null 2>&1 &
-    echo $! > $SERVER/opencore.pid
-    ;;  
-  
-  stop)  
-    kill `cat $SERVER/opencore.pid`
-    rm -rf $SERVER/opencore.pid
-    ;;  
-  
-  restart)  
-    $0 stop  
-    sleep 1  
-    $0 start  
-    ;;  
-  
-  *)  
-    echo "Usage: run.sh {start|stop|restart}"  
-    ;;  
-  
-esac 
+case "$1" in
+
+  start)
+    nohup $JAVA_HOME/bin/java $JAVA_PARAMS $DIAMOND_OPTS -jar $JARNAME.jar -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=63323,server=y,suspend=n >/dev/null 2>&1 &
+    echo $! > $SERVER/$JARNAME.pid
+    ;;
+
+  stop)
+    kill -9 `cat $SERVER/$JARNAME.pid`
+    rm -rf $SERVER/$JARNAME.pid
+    ;;
+
+  restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+
+  *)
+    echo "Usage: run.sh {start|stop|restart}"
+    ;;
+
+esac
 exit 0
