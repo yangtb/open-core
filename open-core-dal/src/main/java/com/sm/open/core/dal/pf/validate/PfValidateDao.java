8fe89c2b6ef4bb3350ec1941a590d3734f3cd02d
yangtb <yang_t_b@163.com>
2018-11-03 23:54:15 +0800

    统一校验service服务

---
+++ open-core-dal/src/main/java/com/sm/open/core/dal/pf/validate/PfValidateDao.java
@@ -0,0 +1,8 @@
+package com.sm.open.core.dal.pf.validate;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PfValidateDao {
+
+}
---
+++ open-core-service-pf/src/main/java/com/sm/open/core/service/service/pf/validate/PfValidateService.java
@@ -0,0 +1,10 @@
+package com.sm.open.core.service.service.pf.validate;
+
+/**
+ * @ClassName: PfValidateService
+ * @Description: 统一校验接口
+ * @Author yangtongbin
+ * @Date 2018/11/3
+ */
+public interface PfValidateService {
+}
